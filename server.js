350c66114658c262dd48405a98e149ff762712ab
leianne louis <leilouis112@gmail.com>
2019-02-22 11:23:45 -0500

    added boiler plate for projectRoutes

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const helmet = require('helmet');
+const logger = require('morgan');
+const server = express();
+const projectsRoutes = require('./routes/projectRoutes');
+
+server.use(helmet());
+server.use(logger());
+server.use('/api/projects', projectsRoutes);
+
+module.exports = server;
